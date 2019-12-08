cd7c074baaca8e1cca67af596959b5be7023e10a
Liam Seymour <liamgseymour@gmail.com>
2019-12-07 22:55:10 -0500

    Ai & sound
    Implemented ai and added sounds for pong hit, win and added a ~sick~ sound track.

--- Pong/Paddle.cs
+++ Pong/Paddle.cs
@@ -13,13 +13,15 @@ namespace Pong
     {
 
         public Vector3 dimensions { get; }
-        private const float SPEED = 10;
+        public Vector3 cpuLocation { get; set; }
+        private const float SPEED = 10f;
+        private const float CPU_SPEED = 3f;
 
         public Paddle(GraphicsDeviceManager graphics, Model model, Vector3 position, Color color, Vector3 dimensions, Effect effect)
                 : base(graphics, model, position, color, 1, effect)
         {
             this.dimensions = dimensions;
-           
+            cpuLocation = position;  
         }
 
 
@@ -81,6 +83,280 @@ namespace Pong
             {
                 position = new Vector3(position.X, -20 + dimensions.Y, position.Z);
             }
+        }
+
+        public void CpuNewLocation(float elapsedMilliseconds, Vector3 ballVelocity, Vector3 ballContact, Vector3 fieldDimensions, float radius)
+        {
+            Vector3 topWallNormal = -Vector3.UnitY;
+
+            Vector3 botWallNormal = Vector3.UnitY;
+
+            Vector3 leftWallNormal = Vector3.UnitX;
+
+            Vector3 rightWallNormal = -Vector3.UnitX;
+
+            Vector3 backWallNormal = Vector3.UnitZ;
+
+            cpuLocation = NextAIPosition(ballVelocity, topWallNormal, botWallNormal, leftWallNormal,  rightWallNormal, backWallNormal,  ballContact, fieldDimensions, radius);
+
+        }
+
+        public void UpdateCPU(float elapsedMilliseconds)
+        {
+            Vector3 cpuMoveVector = cpuLocation - position;
+            cpuMoveVector.Normalize();
+
+            if (position != cpuLocation)
+            {
+                position += new Vector3(cpuMoveVector.X, cpuMoveVector.Y, 0) * elapsedMilliseconds * CPU_SPEED;
+            }
+
+            if (position.X >= 20 - dimensions.X)
+            {
+                position = new Vector3(20 - dimensions.X, position.Y, position.Z);
+            }
+
+            if (position.X <= -20 + dimensions.X)
+            {
+                position = new Vector3(-20 + dimensions.X, position.Y, position.Z);
+            }
+
+            if (position.Y >= 20 - dimensions.Y)
+            {
+                position = new Vector3(position.X, 20 - dimensions.Y, position.Z);
+            }
+
+            if (position.Y <= -20 + dimensions.Y)
+            {
+                position = new Vector3(position.X, -20 + dimensions.Y, position.Z);
+            }
+        }
+
+        private Vector3 NextAIPosition(Vector3 velocity, Vector3 topNormal, Vector3 botNormal,  Vector3 leftNormal, Vector3 rightNormal, Vector3 backNormal, Vector3 startingPoint, Vector3 fieldDimensions, float radius)
+        {
+            float t = 0;
+            float tZ = 0;
+            bool foundPosition = false;
+            bool useTemp = false;
+
+            Vector3 tempPoint = Vector3.Zero;
+            Vector3 nextPoint = Vector3.Zero;
+            float buffer = .2f;
+            while (!foundPosition)
+            {
+                tZ = ((-fieldDimensions.Z + buffer + radius) - startingPoint.Z) / velocity.Z;
+                tempPoint = startingPoint + velocity * tZ;
+
+                if(Vector3.Dot(velocity, topNormal) < 0 && Vector3.Dot(velocity,leftNormal) < 0)
+                {
+                    float tTop = ((fieldDimensions.Y - buffer - radius) - startingPoint.Y) / velocity.Y;
+                    float tLeft = t = ((-fieldDimensions.X + buffer + radius) - startingPoint.X) / velocity.X;
+                    if(tTop <= tLeft)
+                    {
+                        nextPoint = startingPoint + (tTop * velocity);
+                        if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+                        {
+                            velocity.X *= -1;
+                            velocity.Y *= -1;
+                        }
+                        else
+                        {
+                            velocity.Y = -Math.Abs(velocity.Y);
+                        }
+                    }
+                    else
+                    {
+                        nextPoint = startingPoint + (tLeft * velocity);
+                        if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+                        {
+                            velocity.X *= -1;
+                            velocity.Y *= -1;
+                        }
+                        else
+                        {
+                            velocity.X = Math.Abs(velocity.X);
+                        }    
+                    }
+                }else if(Vector3.Dot(velocity, topNormal) < 0 && Vector3.Dot(velocity, rightNormal) < 0)
+                {
+                    float tTop = ((fieldDimensions.Y - buffer - radius) - startingPoint.Y) / velocity.Y;
+                    float tRight = ((fieldDimensions.X - buffer - radius) - startingPoint.X) / velocity.X;
+                    if (tTop <= tRight)
+                    {
+                        nextPoint = startingPoint + (tTop * velocity);
+                        if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+                        {
+                            velocity.X *= -1;
+                            velocity.Y *= -1;
+                        }
+                        else
+                        {
+                            velocity.Y = -Math.Abs(velocity.Y);
+                        }
+                    }
+                    else
+                    {
+                        nextPoint = startingPoint + (tRight * velocity);
+                        if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+                        {
+                            velocity.X *= -1;
+                            velocity.Y *= -1;
+                        }
+                        else
+                        {
+                            velocity.X = -Math.Abs(velocity.X);
+                        }
+                    }
+                }else if (Vector3.Dot(velocity, botNormal) < 0 && Vector3.Dot(velocity, leftNormal) < 0){
+                    float tBot = ((-fieldDimensions.Y + buffer + radius) - startingPoint.Y) / velocity.Y;
+                    float tLeft = t = ((-fieldDimensions.X + buffer + radius) - startingPoint.X) / velocity.X;
+                    if (tBot <= tLeft)
+                    {
+                        nextPoint = startingPoint + (tBot * velocity);
+                        if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+                        {
+                            velocity.X *= -1;
+                            velocity.Y *= -1;
+                        }
+                        else
+                        {
+                            velocity.Y = Math.Abs(velocity.Y);
+                        }
+                    }
+                    else
+                    {
+                        nextPoint = startingPoint + (tLeft * velocity);
+                        if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+                        {
+                            velocity.X *= -1;
+                            velocity.Y *= -1;
+                        }
+                        else
+                        {
+                            velocity.X = Math.Abs(velocity.X);
+                        }
+                    }
+                }else if (Vector3.Dot(velocity, botNormal) < 0 && Vector3.Dot(velocity, rightNormal) < 0)
+                {
+                    float tBot = ((-fieldDimensions.Y + buffer + radius) - startingPoint.Y) / velocity.Y;
+                    float tRight = ((fieldDimensions.X - buffer - radius) - startingPoint.X) / velocity.X;
+                    if (tBot <= tRight)
+                    {
+                        nextPoint = startingPoint + (tBot * velocity);
+                        if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+                        {
+                            velocity.X *= -1;
+                            velocity.Y *= -1;
+                        }
+                        else
+                        {
+                            velocity.Y = Math.Abs(velocity.Y);
+                        }
+                    }
+                    else
+                    {
+                        nextPoint = startingPoint + (tRight * velocity);
+                        if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+                        {
+                            velocity.X *= -1;
+                            velocity.Y *= -1;
+                        }
+                        else
+                        {
+                            velocity.X = -Math.Abs(velocity.X);
+                        }
+                    }
+                }
+                else if (Vector3.Dot(velocity, topNormal) < 0)
+                {
+                    t = ((fieldDimensions.Y - buffer - radius) -startingPoint.Y) / velocity.Y;
+                    nextPoint = startingPoint + (t * velocity);
+                    if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+                    {
+                        velocity.X *= -1;
+                        velocity.Y *= -1;
+                    }
+                    else
+                    {
+                        velocity.Y = -Math.Abs(velocity.Y);
+                    }     
+                    Console.WriteLine("ggggg");
+                }
+                else if (Vector3.Dot(velocity, botNormal) < 0)
+                {  
+                    t = ((-fieldDimensions.Y + buffer + radius) - startingPoint.Y) / velocity.Y;
+                    nextPoint = startingPoint + (t * velocity);
+                    if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+                    {
+                        velocity.X *= -1;
+                        velocity.Y *= -1;
+                    }
+                    else
+                    {
+                        velocity.Y = Math.Abs(velocity.Y);
+                    } 
+                }
+                else if (Vector3.Dot(velocity, leftNormal) < 0)
+                {
+                    t = ((-fieldDimensions.X + buffer + radius) - startingPoint.X) / velocity.X;
+                    nextPoint = startingPoint + (t * velocity);
+                    Console.WriteLine("Our velocity is: " + velocity.X);
+                    if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+                    {
+                        velocity.X *= -1;
+                        velocity.Y *= -1;
+                    }
+                    else
+                    {
+                        velocity.X = Math.Abs(velocity.X);
+                    }  
+                }
+                else if (Vector3.Dot(velocity, rightNormal) < 0)
+                {
+                    t = ((fieldDimensions.X - buffer - radius) - startingPoint.X) / velocity.X;
+                    nextPoint = startingPoint + (t * velocity);
+                    if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+                    {
+                        velocity.X *= -1;
+                        velocity.Y *= -1;
+                    }
+                    else
+                    {
+                        velocity.X = -Math.Abs(velocity.X);
+                    }               
+                }
+                else
+                {
+                    foundPosition = true;
+                }
+                if (nextPoint.Z <= -fieldDimensions.Z)
+                {
+                    foundPosition = true;
+                    useTemp = true;
+                }             
+                startingPoint = nextPoint;
+            }
+            if(useTemp != true)
+            {
+                return nextPoint;
+            }
+            else
+            {
+                return tempPoint;
+            }
+            
+           
+            
+        }
+
+        private Vector3 CheckVelocity(Vector3 velocity)
+        {
+            if (Math.Abs(velocity.X) == Math.Abs(velocity.Y))
+            {
+                velocity.X *= -1;
+                velocity.Y *= -1;
+            }
+            return velocity;
         }
 
         public void Draw(Matrix view, Matrix projection, Vector3 lightPosition, Color lightColor, Vector3 cameraPosition)
